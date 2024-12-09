d409f7a415497ee84844ba67c0bdfe56553e52a6
hanqiaoyu <hanqiaoyu@kuaishou.com>
2024-12-09 16:48:12 +0800

    PR test

--- src/main/java/team/tjusw/elmboot/util/CommonUtil.java
+++ src/main/java/team/tjusw/elmboot/util/CommonUtil.java
@@ -5,12 +5,16 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class CommonUtil {
+	
 
 	public static String getCurrentDate() {
 		Date date = new Date();
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
 		String dataStr = formatter.format(date);
 		return dataStr;
+	}
+	public static String pullRequestTest(){
+		return "pull request test";
 	}
 
 	public static String getNextData(int time) {
